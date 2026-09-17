e5f6323cfd4cdf10790cbce6127a0a68172c424a
aguilar.christoph520 <aguilar.christoph520@student.ccm.edu>
2026-09-17 11:40:41 -0400

    complet problem 1

--- interest_earned.py
+++ interest_earned.py
@@ -1,3 +1,11 @@
-Deposit_Amount = int(input("How much money will you deposit?"))
-Interest_Rate = int(input("Enter Interest Rate: "))
+Principal = int(input("How much money will you deposit?"))
+Interest_Rate = float(input("Enter Interest Rate: "))
 Compound = int(input("Enter Times Compounded: "))
+Final_Balance = Principal*(1+Interest_Rate/Compound)**4
+Interest = Final_Balance - Principal
+
+print("Interest Rate: ", Interest_Rate,"%")
+print("Times Compounded:", Compound)
+print("Principle: $", Principal)
+print("Interest: $", Interest)
+print("Amount in savings: $", Final_Balance)
